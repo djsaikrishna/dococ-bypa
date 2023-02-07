e901a6e53967ba15c84fd8bd1ea107b76d7e6c84
saikrishnaegcc2 <90199989+saikrishnaegcc2@users.noreply.github.com>
2023-02-07 23:44:01 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,17 +1,17 @@
 import os
 import logging
 class Config:
-    API_ID = int(os.environ.get("API_ID", ""))
-    API_HASH = os.environ.get("API_HASH", "")       
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-    Channel_id = os.environ.get("Channel_id", "")
+    API_ID = int(os.environ.get("API_ID", "15882573"))
+    API_HASH = os.environ.get("API_HASH", "dddd64edfc5326e4a35e448347b83e2d")       
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "5506649317:AAGwpJgnkBNOKQLCefciDroCPXsPtyYUi_A")
+    Channel_id = os.environ.get("Channel_id", "-1001807232349")
     Drivebuzz_crypt = os.environ.get("Drivebuzz_crypt", "")
     Drivefire_crypt = os.environ.get("Drivefire_crypt", "")
     Jiodrive_crypt = os.environ.get("Jiodrive_crypt", "")
     Gadrive_crypt = os.environ.get("Gadrive_crypt", "")
     Kolop_crypt = os.environ.get("Kolop_crypt", "")
     Katdrive_crypt = os.environ.get("Katdrive_crypt", "")
-    Gtot_crypt = os.environ.get("Gtot_crypt", "")
+    Gtot_crypt = os.environ.get("Gtot_crypt", "NUtOdnFnSnl0VUpScFFkSTNqaDhaeVAxeDViaXp3aHpla3M3L2FhcGNvQT0%3D")
     Appdrive_email = os.environ.get("Appdrive_email", "")
     Appdrive_password = os.environ.get("Appdrive_password", "")
     Hubdrive_crypt = os.environ.get("Hubdrive_crypt", "")
